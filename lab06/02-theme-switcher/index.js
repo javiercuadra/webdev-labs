bac0842aec8979cffd1d7d002a938f0a650a0b61
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2022-05-05 21:43:41 -0500

    complete theme switcher

--- lab06/02-theme-switcher/index.js
+++ lab06/02-theme-switcher/index.js
@@ -1,7 +1,32 @@
 /*
-    Hints: 
-    1. Attach click event handlers to all four of the 
+    Hints:
+    1. Attach click event handlers to all four of the
        buttons (#default, #ocean, #desert, and #high-contrast).
-    2. Modify the className property of the body tag 
+    2. Modify the className property of the body tag
        based on the button that was clicked.
 */
+
+const applyDefaultTheme = () => {
+   // alert('make smaller!');
+   document.querySelector("body").className = ""
+};
+
+const applyOceanTheme = () => {
+   // alert('make smaller!');
+   document.querySelector("body").className = "ocean"
+};
+
+const applyDesertTheme = () => {
+   // alert('make smaller!');
+   document.querySelector("body").className = "desert"
+};
+
+const applyHighContrastTheme = () => {
+   // alert('make smaller!');
+   document.querySelector("body").className = "high-contrast"
+};
+
+document.getElementById('default').addEventListener('click', applyDefaultTheme);
+document.getElementById('ocean').addEventListener('click', applyOceanTheme);
+document.getElementById('desert').addEventListener('click', applyDesertTheme);
+document.getElementById('high-contrast').addEventListener('click', applyHighContrastTheme);
