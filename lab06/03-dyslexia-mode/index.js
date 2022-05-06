bb422ac8650afb294306a3a1da8b702b63e6bd24
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2022-05-05 22:21:50 -0500

    made dyslexia-mode persistent

--- lab06/03-dyslexia-mode/index.js
+++ lab06/03-dyslexia-mode/index.js
@@ -2,23 +2,17 @@
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
 */
+const dyslexiaToggleButton =  document.getElementById('dyslexia-toggle')
+const isDyslexiaModeOn = window.localStorage.getItem('dyslexic');
 
-isDyslexiaModeOn = false
-
-
-const toggleDyslexiaMode = () => {
-
-  isDyslexiaModeOn = !isDyslexiaModeOn
-
-  if (!isDyslexiaModeOn){
-    document.querySelector("body").className = ""
-  }
-  else{
-    document.querySelector("body").className = "dyslexia-mode"
-  }
-
-  console.log(`Is dyslexia mode on? ${isDyslexiaModeOn}`)
+if(isDyslexiaModeOn) {
+  document.body.classList.add('dyslexia-mode');
+  dyslexiaToggleButton.setAttribute('aria-pressed', 'true');
 }
 
-
-document.getElementById('dyslexia-toggle').addEventListener('click', toggleDyslexiaMode);
+dyslexiaToggleButton.addEventListener('click', (e) => {
+  let pressed = e.target.getAttribute('aria-pressed') === 'true';
+  e.target.setAttribute('aria-pressed', String(!pressed));
+  document.body.classList.toggle('dyslexia-mode');
+  window.localStorage.setItem('dyslexic', String(!pressed));
+});
