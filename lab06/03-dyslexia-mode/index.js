b85931e08d0b519ee1bce93160878ab085e998e3
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2022-05-05 21:51:03 -0500

    Added dyslexia mode toggle button functionality

--- lab06/03-dyslexia-mode/index.js
+++ lab06/03-dyslexia-mode/index.js
@@ -1,4 +1,24 @@
-/* 
+/*
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
 */
+
+isDyslexiaModeOn = false
+
+
+const toggleDyslexiaMode = () => {
+
+  isDyslexiaModeOn = !isDyslexiaModeOn
+
+  if (!isDyslexiaModeOn){
+    document.querySelector("body").className = ""
+  }
+  else{
+    document.querySelector("body").className = "dyslexia-mode"
+  }
+
+  console.log(`Is dyslexia mode on? ${isDyslexiaModeOn}`)
+}
+
+
+document.getElementById('dyslexia-toggle').addEventListener('click', toggleDyslexiaMode);
