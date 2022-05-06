3908702b530235b34cf318f017f3557039182988
Javier Francisco Cuadra <32556567+javiercuadra@users.noreply.github.com>
2022-05-05 21:29:52 -0500

    complete font-switcher

--- lab06/01-font-switcher/index.js
+++ lab06/01-font-switcher/index.js
@@ -1,12 +1,24 @@
+const RESIZE_MAGNITUDE = 10
+
+const resizeElement = (element, resizeAmount) => {
+   let divElement = document.querySelector(element)
+   let divElementFontSize = parseFloat(window.getComputedStyle(divElement, null).getPropertyValue('font-size'));
+
+   divElement.style.fontSize = (divElementFontSize + resizeAmount) + "px"
+}
+
 const makeBigger = () => {
-   alert('make bigger!');
+   // alert('make bigger!');
+   resizeElement("div.content", RESIZE_MAGNITUDE)
+   resizeElement("h1", RESIZE_MAGNITUDE)
 };
 
 const makeSmaller = () => {
-   alert('make smaller!');
+   // alert('make smaller!');
+   resizeElement("div.content", -RESIZE_MAGNITUDE)
+   resizeElement("h1", -RESIZE_MAGNITUDE)
 };
 
-/*
-document.querySelector(???).addEventListener('click', makeBigger);
-document.querySelector(???).addEventListener('click', makeSmaller);
-*/
+
+document.querySelector('#a1').addEventListener('click', makeBigger);
+document.querySelector('#a2').addEventListener('click', makeSmaller);
